cbc58787d53f3fb6949a018a8c38280e2e8b6191
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-12-30 13:43:48 +0530

    Update Longest common subsequence.java

--- Longest common subsequence.java
+++ Longest common subsequence.java
@@ -1,29 +1,23 @@
 class Solution {
     public int lcs(int m, int n, String text1, String text2, int dp[][]){
-      if(m==0 || n==0){
-          return 0;
-      }
-      if(dp[m][n]!=-1){
+       for(int i=1; i<=m; i++){
+           for(int j=1; j<=n; j++){
+               if(text1.charAt(i-1)==text2.charAt(j-1)){
+                   dp[i][j]=1+dp[i-1][j-1];
+               }
+               else{
+                   dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+               }
+           }
+       }
           return dp[m][n];
       }
-      if(text1.charAt(m-1)==text2.charAt(n-1)){
-          dp[m][n]=1+lcs(m-1,n-1,text1,text2,dp);
-          return dp[m][n];
-      }
-      else{
-          dp[m][n]=Math.max(lcs(m,n-1,text1,text2,dp),lcs(m-1,n,text1,text2,dp));
-          return dp[m][n];
-      }
-    }
-    public int longestCommonSubsequence(String text1, String text2) {
+    
+    public int longestCommonSubsequence(String text1, String text2){
      int m=text1.length();
      int n=text2.length();
      int dp[][]=new int[m+1][n+1];
-     for(int i=1; i<=m; i++){
-         for(int j=1; j<=n; j++){
-             dp[i][j]=-1;
-         }
-     }
+    
         int ans=lcs(m,n,text1,text2,dp);
         return ans;
     }
